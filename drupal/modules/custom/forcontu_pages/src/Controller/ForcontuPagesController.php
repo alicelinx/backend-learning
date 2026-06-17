c3d12e978e3fbb17bb1c61ad1d70cfa8a056d5b3
Alice Lin <aliclin@forcontu.com>
2026-06-17 15:28:26 -0600

    17.5a - add tabs, sub-tabs, and extra tab for Forcontu Pages module

--- drupal/modules/custom/forcontu_pages/src/Controller/ForcontuPagesController.php
+++ drupal/modules/custom/forcontu_pages/src/Controller/ForcontuPagesController.php
@@ -154,4 +154,42 @@ class ForcontuPagesController extends ControllerBase {
     ];
     return $output;
   }
+
+  public function tab1() {
+    return [
+      '#markup' => '<p>' . $this->t('This is the content of Tab 1') . '</p>',
+    ];
+  }
+
+  public function tab2() {
+    return [
+      '#markup' => '<p>' . $this->t('This is the content of Tab 2') . '</p>',
+    ];
+  }
+
+  public function tab3() {
+    return [
+      '#markup' => '<p>' . $this->t('This is the content of Tab 3') . '</p>',
+    ];
+  }
+
+  public function tab3a() {
+    return [
+      '#markup' => '<p>' . $this->t('This is the content of Tab 3a') . '</p>',
+    ];
+  }
+
+  public function tab3b() {
+    return [
+      '#markup' => '<p>' . $this->t('This is the content of Tab 3b') . '</p>',
+    ];
+  }
+
+  public function extratab() {
+    return [
+      '#markup' => '<p>' . $this->t('This is the content of extratab') . '</p>',
+    ];
+  }
+
+  
 }
