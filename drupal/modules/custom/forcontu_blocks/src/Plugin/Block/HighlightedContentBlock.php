0e7fe0cfb6ab47bb3b2930da9cbaae4269be6538
Alice Lin <aliclin@forcontu.com>
2026-08-10 13:26:00 -0600

    24.5a - inject current_user, database and entity_type.manager services

--- drupal/modules/custom/forcontu_blocks/src/Plugin/Block/HighlightedContentBlock.php
+++ drupal/modules/custom/forcontu_blocks/src/Plugin/Block/HighlightedContentBlock.php
@@ -4,6 +4,11 @@ namespace Drupal\forcontu_blocks\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Provides the HighlightedContent block.
@@ -13,11 +18,77 @@ use Drupal\Core\Form\FormStateInterface;
  *    admin_label = @Translation("Highlighted Content")
  * )
  */
-class HighlightedContentBlock extends BlockBase {
+class HighlightedContentBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  protected $database;
+  protected $currentUser;
+  protected $entityTypeManager;
+
+  public function __construct(array $configuration,
+                              $plugin_id,
+                              $plugin_definition,
+                              AccountInterface $current_user,
+                              Connection $database,
+                              EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+    $this->database = $database;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  public static function create(ContainerInterface $container,
+                                array $configuration,
+                                $plugin_id,
+                                $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('database'),
+      $container->get('entity_type.manager'),
+    );
+  }
+
   public function build() {
-    return [
-      '#markup' => '<span>' . $this->t('Highlighted') . '</span>'
+    $node_number = $this->configuration['node_number'];
+    $block_message = $this->configuration['block_message'];
+
+    // Display the configured block message
+    $build[] = [
+      '#markup' => '<h3>' . $this->t($block_message) . '</h3>',
     ];
+
+    // Query the database for highlighted nodes
+    $result = $this->database->select('forcontu_node_highlighted', 'f')
+      ->fields('f', ['nid'])
+      ->condition('highlighted', 1)
+      ->orderBy('nid', 'DESC')
+      ->range(0, $node_number)
+      ->execute();
+
+    // Initialize the list and node storage
+    $list = [];
+    $node_storage = $this->entityTypeManager->getStorage('node');
+
+    // Load each node and build a list of links
+    foreach($result as $record) {
+      $node = $node_storage->load($record->nid);
+      $list[] = $node->toLink($node->getTitle())->toRenderable();
+    }
+
+    if (empty($list)) {
+      $build[] = [
+        '#markup' => '<h3>' . $this->t('No results found') . '</h3>',
+      ];
+    } else {
+    // Render the list of highlighted nodes
+      $build[] = [
+        '#theme' => 'item_list',
+        '#items' => $list,
+        '#cache' => ['max-age' => 0],
+      ];
+    }
+    return $build;
   }
 
   public function defaultConfiguration() {
